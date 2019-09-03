31c063717df4be236b94a65a416c26a138ccf371
zachm7115 <zachm7115@gmail.com>
2019-09-03 15:00:09 -0400

    Added main template from mr hopps

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main extends JPanel{
+
+    public static final int WIDTH=600, HEIGHT=600;
+    private Timer timer;
+
+
+
+    public Main(){
+
+
+        timer = new Timer(1000 / 60, e -> update());
+        timer.start();
+        setKeyListener();
     }
+
+    public void update() {
+
+
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+
+
+
+    }
+
+    public void setKeyListener(){
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setBounds(0, 0, WIDTH, HEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main();
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
+
 }
