a7338b1c2d5957a3f61ea488c5b4f585f018f257
zachm7115 <zachm7115@gmail.com>
2019-09-23 11:57:07 -0400

    updated boss so code runs ahahh

--- src/Boss.java
+++ src/Boss.java
@@ -1,3 +1,10 @@
 public class Boss extends Sprite{
 
+    public Boss(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public Boss(int x, int y, int width, int height, int levelShown) {
+        super(x, y, width, height, levelShown);
+    }
 }
